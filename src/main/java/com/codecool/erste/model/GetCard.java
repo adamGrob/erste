ba058836c80edda141be41bd42898a4c718ea325
adamGrob <ad4mgr0b@gmail.com>
2018-09-02 15:47:59 +0200

    Created the GetCard class. This is the class I use the build the json response object for getting the card information.

---
+++ src/main/java/com/codecool/erste/model/GetCard.java
@@ -0,0 +1,19 @@
+package com.codecool.erste.model;
+
+import java.util.List;
+
+public class GetCard extends TransCard {
+
+    private boolean disabled;
+
+    public GetCard(){}
+
+    public GetCard(String cardType, String cardNumber, String validThru, boolean disabled, String owner, List<ContactInfo> contactInfos) {
+        this.cardType = cardType;
+        this.cardNumber = cardNumber;
+        this.validThru = validThru;
+        this.disabled = disabled;
+        this.owner = owner;
+        this.contactInfos = contactInfos;
+    }
+}
