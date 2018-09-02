ae2fd1de945c1750c172b68a97e41b4970f50923
adamGrob <ad4mgr0b@gmail.com>
2018-09-02 19:11:50 +0200

    Created the ValidationResult class.

---
+++ src/main/java/com/codecool/erste/model/validationCardModels/ValidationResult.java
@@ -0,0 +1,9 @@
+package com.codecool.erste.model.validationCardModels;
+
+public class ValidationResult {
+    private Valid result;
+
+    public ValidationResult(Valid result) {
+        this.result = result;
+    }
+}
