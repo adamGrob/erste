1c5dc97567c5e77ac58adfaaea5d3167b92466f7
adamGrob <ad4mgr0b@gmail.com>
2018-09-02 19:13:59 +0200

    Created the main class for the application.

---
+++ src/main/java/com/codecool/erste/ErsteApplication.java
@@ -0,0 +1,8 @@
+package com.codecool.erste;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("bank")
+public class ErsteApplication extends Application {
+}
